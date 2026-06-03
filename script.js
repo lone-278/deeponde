ecae9f0b14e11f63e1cfb40fcc9666cc2054b954
lone-278 <animer26@gmail.com>
2026-06-04 00:57:41 +0900

    스와이퍼 오류

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
+})
