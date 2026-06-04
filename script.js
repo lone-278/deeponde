8699000f1c32331d8c714c3b3d6d28f7b3563e19
lone-278 <animer26@gmail.com>
2026-06-04 22:31:58 +0900

    플렉스박스

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
-  var swiper = new Swiper(".mySwiper", {
+  var swiper = new Swiper(".type-1", {
     slidesPerView: 2,
     spaceBetween: 20,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    }
+  });
+
+    var swiper = new Swiper(".type-2", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
     }
   });
 })
